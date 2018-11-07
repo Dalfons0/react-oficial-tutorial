6a2f0f4c2ef233857f252132a2f4bd429c38201d
CORP\dalfonso <david.alfonso-garcia@capgemini.com>
2018-11-07 16:04:52 +0100

    [REFACTOR] Component and method order & add styles to toggle button

--- src/index.js
+++ src/index.js
@@ -11,6 +11,19 @@ function Square(props) {
   );
 }
 
+function ToggleButton(props) {
+  return (
+    <div className="toggle-button">
+      <button className={props.ordering ? 'toggled' : ''} onClick={() => props.onClick(true)} disabled={props.ordering}>
+        Ascending
+      </button>
+      <button className={props.ordering ? '' : 'toggled'} onClick={() => props.onClick(false)} disabled={!props.ordering}>
+        Descending
+      </button>
+    </div>
+  );
+}
+
 class Board extends React.Component {
   renderSquare(i) {
     return (
@@ -38,41 +51,6 @@ class Board extends React.Component {
   render() {
     return <div>{this.renderBoard()}</div>;
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return lines[i];
-    }
-  }
-  return null;
-}
-
-function ToggleButton(props) {
-  return (
-    <div className="toggle-button">
-      <button onClick={() => props.onClick(true)} disabled={props.ordering}>
-        {' '}
-        Ascending{' '}
-      </button>
-      <button onClick={() => props.onClick(false)} disabled={!props.ordering}>
-        {' '}
-        Descending{' '}
-      </button>
-    </div>
-  );
 }
 
 class Game extends React.Component {
@@ -176,3 +154,23 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById('root'));
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return lines[i];
+    }
+  }
+  return null;
+}
